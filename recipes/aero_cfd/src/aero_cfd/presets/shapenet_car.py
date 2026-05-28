ca669437c382aebcbff85471cb1007b39d8d6813
Andy Ye <andyye2@illinois.edu>
2026-05-28 22:22:05 +0800

    fix: exclude missing ShapeNet surface area

--- recipes/aero_cfd/src/aero_cfd/presets/shapenet_car.py
+++ recipes/aero_cfd/src/aero_cfd/presets/shapenet_car.py
@@ -73,7 +73,7 @@ class ShapeNetCarPreset(AeroCFDPreset):
 
     @property
     def excluded_properties(self) -> set[str]:
-        return {"surface_friction", "volume_pressure", "volume_vorticity"}
+        return {"surface_area", "surface_friction", "volume_pressure", "volume_vorticity"}
 
     def target_properties(self) -> list[str]:
         return ["surface_pressure_target", "volume_velocity_target"]
